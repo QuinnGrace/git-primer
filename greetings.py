c3d798f959bb0cf860aa1f01882ee3c8ab591bc5
Oluwasaanu <akinniyijoseph@gmail.com>
2023-09-07 13:08:56 +0100

    modified the greetings.py file into proper syntax format

--- greetings.py
+++ greetings.py
@@ -3,10 +3,11 @@ from datetime import datetime
 name = input("What is your name? ")
 hour = datetime.now().time().hour
 greeting = (
-    "Good morning"
-    if 4 <= hour < 12
-    else "Good afternoon"
-    if 12 <= hour < 16
-    else "Good night"
+    if 4 <= hour < 12:
+        "Good morning"
+    elif 12 <= hour < 16:
+        "Good afternoon"
+    else:
+        "Good night"
 )
 print(f"{greeting}, {name}!")
